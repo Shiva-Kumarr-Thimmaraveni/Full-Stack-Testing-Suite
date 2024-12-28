21122b29c10ef71821e8a1aa413b8462b1b8ba8d
Shiva-Kumarr-Thimmaraveni <153267857+Shiva-Kumarr-Thimmaraveni@users.noreply.github.com>
2024-12-28 17:43:37 +0530

    Update tailwind.config.js

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,3 +1,4 @@
+import scrollbar from 'tailwind-scrollbar'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
@@ -31,6 +32,6 @@ export default {
     },
   },
   plugins: [
-    require('tailwind-scrollbar'), // Add the scrollbar plugin
+    scrollbar
   ],
 }
